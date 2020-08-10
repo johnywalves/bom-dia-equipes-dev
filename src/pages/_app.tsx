ff55f5c07375f8f606627b6b8286e5e07525d504
Johny W. Alves <johnywalves@gmail.com>
2020-08-10 11:46:40 -0300

    :mag: Adicionando thumbnail para SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,10 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
+          name="og:image"
+          content="http://bomdiaequipesdev.com.br/img/thumbnail.png"
+        />
+        <meta
           name="description"
           content="Uma maneira criativa para saudar seus colegas devenvolvedores toda manhã"
         />
