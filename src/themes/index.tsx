cd6d75789c37c19ba8b1c19325c06f823a7774f8
Johny W. Alves <johnywalves@gmail.com>
2022-10-11 17:54:38 -0300

    :speech_balloon: Adição de mensagem

--- src/themes/index.tsx
+++ src/themes/index.tsx
@@ -49,7 +49,8 @@ export const themeFunny: FuncTheme = () => [
 export const themeHoly: FuncTheme = () => [
   'irmãs e irmãos, já agradeceram por acordarem bem e saudáveis neste belo dia? Que ele veja cada vez mais iluminado',
   'sejam bem-vindos, pessoas de luz para mais um abençoado dia',
-  'consagradas e consagrados, o deus que habita em mim cumprimenta o deus que habita em vocês para lhes desejarem boas energias e ótimos eventos vindoros'
+  'consagradas e consagrados, o deus que habita em mim cumprimenta o deus que habita em vocês para lhes desejarem boas energias e ótimos eventos vindoros',
+  'bom dia, santas e santos, que o louvor do altissimo abençoe a todos, para mais um dia de graça e iluminação'
 ]
 
 export const themeTokusatsu: FuncTheme = () => [
